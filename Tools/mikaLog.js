fe098e3aa1990e3c7fe9f2777e9b6afc4cc91644
MikackermanDev <122027129+MikackermanDev@users.noreply.github.com>
2023-05-28 00:42:15 +0200

    distinction : app.log + err.log

--- Tools/mikaLog.js
+++ Tools/mikaLog.js
@@ -7,18 +7,21 @@ const util = require("util");
 
 // Contient le chemin du répertoire à créer
 const logCheminDossier = path.join(__dirname, "..", "Logs");
-// path.join pour joindre le nom du fichier au nom du répertoire
-const logCheminFichier = path.join(logCheminDossier, "mikaApp.log");
+console.log(logCheminDossier);
 
+// Créer le dossier s'il n'existe pas
+if (!fs.existsSync(logCheminDossier)) {
+	fs.mkdirSync(logCheminDossier);
+}
 // on définit le format d'affichage de l'horodatage pour NodeJS avec date-fns
 // const mesOptions = "eee dd MMM yyyy HH:mm:ss.SSS";
 
 // on définit le format d'affichage de l'horodatage
 const mesOptions = {
-	weekday: "short",
+	// weekday: "short",
 	day: "2-digit",
-	month: "short",
-	year: "numeric",
+	month: "2-digit",
+	year: "2-digit",
 	hour: "2-digit",
 	minute: "2-digit",
 	second: "2-digit",
@@ -28,36 +31,42 @@ const mesOptions = {
 
 // fonction pour le log personnalisé
 function createMikaLog(fichierOrigine) {
-	return function (...args) {
+	return function (arg) {
 		// Extraction du chemin relatif du fichier d'origine grace à path.relative
 		const cheminRelatif = path.relative(__dirname, fichierOrigine);
 		// Création de l'horodatage
 		const date = new Date();
 		// si NodeJS avec date-fns
 		// const horodatage = format(date, mesOptions);
-		const horodatage =
-			date.toLocaleString("fr-FR", mesOptions).replace(",", " à") +
-			"." +
-			date.getMilliseconds().toString().padStart(3, "0");
+		const horodatage = date.toLocaleString("fr-FR", mesOptions).replace(",", " à");
+		+"." + date.getMilliseconds().toString().padStart(3, "0");
 		// Mise en forme du message de journalisation
-		let logMessage = `${horodatage} \t -> issu de ${cheminRelatif} \t -> ${util.format(
-			...args
-		)}\n`;
+		let logMessage = `${horodatage} -> ${cheminRelatif} \t -> ${util.format(arg)}\n`;
 		// Affichage du message dans la console
 		// ATTENTION PEU FAIRE CONFLIT AVEC CONSOLE.LOG
 		// console.log(logMessage);
-		// Ajout du message au fichier de log
-		fs.appendFile(logCheminFichier, logMessage, { flag: "a" }, (err) => {
-			if (err) {
-				console.error(`Une erreur s'est produite voici le log: ${err}`);
-			}
-		});
-	};
-}
 
-// Créer le dossier s'il n'existe pas
-if (!fs.existsSync(logCheminDossier)) {
-	fs.mkdirSync(logCheminDossier);
+		// Détermination du nom du fichier de log en fonction du type de l'argument
+		let logFileName;
+		if (arg instanceof Error) {
+			// Si l'argument est une instance de Error, on utilise errLog.txt
+			logFileName = "mikaErr.log";
+		} else {
+			// Sinon, on utilise un nom par défaut
+			logFileName = "mikaApp.log";
+		}
+		// Ajout du message au fichier de log
+		fs.appendFile(
+			path.join(logCheminDossier, logFileName),
+			logMessage,
+			{ flag: "a" },
+			(err) => {
+				if (err) {
+					console.error(`Une erreur s'est produite voici le log: ${err}`);
+				}
+			}
+		);
+	};
 }
 
 module.exports = createMikaLog;
